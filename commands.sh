a1a5110e0b77a9ac64d4e5a6c4f75eb5fe6da419
Andrew Pong <a@awp.io>
2016-03-03 17:05:12 -0800

    pillage the old cruft

--- commands.sh
+++
@@ -1,2 +0,0 @@
-gulp serve:build
-jspm bundle-sfx app build/build.js --minify --inject
--- src/app.js
+++
@@ -1,4 +0,0 @@
-let incidentForm = {}
-incidentForm.guid = 'abcdefg';
-import { populateDropdown } from 'lib.js';
-populateDropdown(incidentForm.guid, 'id', 'cities.json');
